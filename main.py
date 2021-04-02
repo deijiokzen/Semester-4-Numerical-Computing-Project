bf2d378babd2dd20b9b020e1edf9869dbad7eed3
deijiokzen <satokoentertainment@gmail.com>
2021-04-02 22:22:15 +0500

    Committed #7

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from sympy import *
 import sympy, numpy as np, math
 from sympy.parsing.sympy_parser import (parse_expr, standard_transformations, implicit_multiplication_application)
 
-def func_bisector(expression, limit_val):
+def func(expression, limit_val):
     expression = expression.translate({ord(c): "**" for c in "^"})
-    Function = parse_expr(expression, local_dict={"log": lambda x: sympy.log(x, 10)}, transformations=(standard_transformations + (implicit_multiplication_application,)))
+    Function = parse_expr(expression,  local_dict={"log": lambda x: sympy.log(x, 10)}, transformations=(standard_transformations + (implicit_multiplication_application,)))
     e = symbols('e')
     Function = Function.subs(e, exp(1))
     symbol_vals = {}
@@ -17,7 +17,7 @@ def func_bisector(expression, limit_val):
     Function = Function.subs(symbol_vals)
     return Function.evalf()
 
-def func_newton(expression, limit_val):
+def funcdifferential(expression, limit_val):
     expression = expression.translate({ord(c): "**" for c in "^"})
     Function = parse_expr(expression, local_dict={"log": lambda x: sympy.log(x, 10),
                                                   "cos": lambda x: sympy.cos(sympy.deg(x)),
@@ -70,41 +70,46 @@ def newton(Expression, L_x, L_y, Eps):
 
 
 
-def bisection(Expression, L_x, L_y, Eps):
-    IVR = abs(func_bisector(Expression, L_x) * func_bisector(Expression, L_y))
+def bisection(Expression, L_x, L_y, Eps, Round_Val):
+    if Round_Val > 12:
+        Round_Val = 12
+    IVR = abs(func(Expression, L_x) * func(Expression, L_y))
     a = 0
-    header=["A", "B", "C=(A-B)/2", "F(C)", "ERROR"]
+    header=["N","A", "B", "C=(A-B)/2", "F(C)", "ERROR"]
     matrix=[]
+    i=-1
     while(1):
+        i+=1
 
         c = (L_x + L_y) / 2
         # print(c,"-",a)
-        # matrix.extend([[L_x, L_y, c, func_bisector(Expression,c), abs(c-a)]])
-        print([L_x, L_y, c, func_bisector(Expression,c), abs(c-a)])
-        if abs(c-a) < Eps or IVR == 0:
+        list =[i,round(L_x,Round_Val), round(L_y,Round_Val), round(c,Round_Val), round(func(Expression,c),Round_Val), round(abs(c-a),Round_Val)]
+        matrix.extend([list])
+        # print([round(L_x,Round_Val), round(L_y,Round_Val), round(c,Round_Val), round(func_bisector(Expression,c),Round_Val), round(abs(c-a),Round_Val)])
+        if abs(c-a) < Eps or IVR == 0 or i==85:
             if abs(c - a) < Eps:
                 print("The error value has reached!\n")
             else:
                 print("IVR has reached!\n")
             break
-        elif func_bisector(Expression,c) < 0:
-            if(func_bisector(Expression,L_x) < 0):
+        elif func(Expression,c) < 0:
+            if(func(Expression,L_x) < 0):
                 L_x=c
             else:
                 L_y=c
 
-        elif func_bisector(Expression,c) > 0:
-            if (func_bisector(Expression, L_x) > 0):
+        elif func(Expression,c) > 0:
+            if (func(Expression, L_x) > 0):
                 L_x = c
             else:
                 L_y = c
         a = c
 
-    row_format ="{:>20}" * (len(header) + 1)
+    row_format ="{:>25}" * (len(header) + 1)
     print(row_format.format("", *header))
-    for team, row in zip(header, matrix):
+    for row in matrix:
         print(row_format.format("", *row))
-    print("Your final value of c is: ", c)
+    print("Your final value of c is: ", c, ", While your rounded off value is: ", round(c,Round_Val))
 
 def regular_falsi(Expression, L_x, L_y, Eps):
     IVR = abs(func_bisector(Expression, L_x) * func_bisector(Expression, L_y))
@@ -165,8 +170,8 @@ def func_convergence(expression, val):
 # L_y = float(sympify(input("Enter value for Upper Limit:").translate({ord(c): "**" for c in "^"})).evalf())
 # Eps = float(sympify(input("Input tolerance value:").translate({ord(c): "**" for c in "^"})).evalf())
 #
-# bisection(Expression,L_x,L_y,Eps)
-regular_falsi("x-0.8-0.2sin x",0,math.pi/2,10**-4)
+bisection("x * cos(x) - 2x^2 +3x -1",1.2,1.3,10**-5,6)
+# regular_falsi("x-0.8-0.2sin x",0,math.pi/2,10**-4)
 # newton("3x - e^(x)",1,2,10**-5)
 # secant("ln(x-1) + cos(x-1)", 1.3,2,10**-5)
 # func_convergence("3/(x(x^(2)-3))", 1)
