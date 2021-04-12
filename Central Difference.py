da277aa28d57b207ea5c80b5b5634fd9d8a7e2f7
deijiokzen <satokoentertainment@gmail.com>
2021-04-12 15:12:15 +0500

    Committed #18

--- Central Difference.py
+++ Central Difference.py
@@ -46,10 +46,12 @@ def stirling_table(n,round_off):
     # for i in range(0, n, 1):
     #     print("\nEnter value for f(x",i,"):")
     #     table[0].append(float(input()))
-    x=1.5
-    xtable = [1,1.3,1.6,1.9,2.1]
-    table=[[0.7651977,0.6200860,0.4554022,0.2818186,0.1103623]]
-
+    # x=1.5
+    # xtable = [1,1.3,1.6,1.9,2.1]
+    # table=[[0.7651977,0.6200860,0.4554022,0.2818186,0.1103623]]
+    x = 0.5437
+    xtable = [0.51,0.52,0.53,0.54,0.55,0.56,0.57]
+    table=[[0.5292437, 0.5378987, 0.5464641, 0.5549392, 0.563323, 0.5716157, 0.57981]]
     if(len(xtable)%2==0):
         print("To acquire function’s values near the middle of a table where central difference interpolation"
                " formulas are applicable in which our new method is very suitable. This method employs"
@@ -65,15 +67,19 @@ def stirling_table(n,round_off):
     matrix=[]
     final_stir= table[0][math.floor(len(xtable)/2)]
     print(final_stir, "+")
+    i=1
     j=0
-    for i in range(1, n-1, 2):
-        matrix.append([table[i][len(table[i])//2-1],table[i][len(table[i])//2], table[i+1][math.floor(len(table[i])//2)-1]])
-        final_stir+=stirling([table[i][len(table[i])//2-1],table[i][len(table[i])//2], table[i+1][math.floor(len(table[i])//2)-1]], j, p)
+    for j in range(0, int(n/2), 1):
+        print(i)
+        matrix.append([round(table[i][len(table[i])//2-1],round_off),round(table[i][len(table[i])//2],round_off), round(table[i+1][math.floor(len(table[i])//2)-1],round_off)])
+        final_stir+=stirling([round(table[i][len(table[i])//2-1],round_off),round(table[i][len(table[i])//2],round_off), round(table[i+1][math.floor(len(table[i])//2)-1],round_off)], j, p)
         j+=1
+        i+=2
+        matrix=[]
 
     return round(final_stir, round_off)
 
 
-print("Your answer for stirling is :",stirling_table(5,7))
+print("Your answer for stirling is :",stirling_table(7,10))
 
 # print(1.49182 + stirling([0.27042,0.33030,0.05988], 0, 0.15) + stirling([0.01086,0.01324,0.00238], 1, 0.15)   )
