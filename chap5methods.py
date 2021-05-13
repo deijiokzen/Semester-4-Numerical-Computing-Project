17dbd68015073ac9a7cb4ec3d48830e4d1f0b1dd
T-Zaid <75293628+T-Zaid@users.noreply.github.com>
2021-05-13 20:58:40 +0500

    some changes

--- chap5methods.py
+++ chap5methods.py
@@ -2,8 +2,7 @@ from main import *
 from func2 import *
 import os
 
-
-def ModifiedEuler():
+def enterdata():
     Expression = input(
         "Enter the Function to Integrate using Modified Euler Method : ")
     h = float(input("Enter h : "))
@@ -12,18 +11,36 @@ def ModifiedEuler():
     y0 = float(input("Enter y("+str(bound1)+") : "))
     fix = int(input("Enter the rounding point digits : "))
 
+    choice = int(input("Which methods do you want to use for Integrating : \n1. Euler.\n2. Modified Euler.\n3. Huen Method.\n4. RK4 Method\n Choice : "))
+    if choice == 1:
+        Euler(Expression, h, n, bound1, bound2, y0, fix)
+    elif choice == 2:
+        ModifiedEuler(Expression, h, n, bound1, bound2, y0, fix)
+    elif choice == 3:
+        Huen(Expression, h, n, bound1, bound2, y0, fix)
+    elif choice == 4:
+        Rk4(Expression, h, n, bound1, bound2, y0, fix)
+    else:
+        print("no choice allocated to this number.")
+
+def ModifiedEuler(Expression, h, n, bound1, bound2, y0, fix):
+    # Expression = input(
+    #     "Enter the Function to Integrate using Modified Euler Method : ")
+    # h = float(input("Enter h : "))
+    # n = bound1 = float(input("From a = "))
+    # bound2 = float(input("To b = "))
+    # y0 = float(input("Enter y("+str(bound1)+") : "))
+    # fix = int(input("Enter the rounding point digits : "))
+
     matrix = []
     while (n <= bound2):
         list = [n, y0]
         n += h
         matrix.append(list)
-        # print(matrix[0][0])
 
     n = bound1+h
     i = int(0)
     while (n <= bound2):
-        # a = matrix[i][0]
-        # print (a)
         k1 = round((h * func2(Expression, matrix[i][0], matrix[i][1])), fix)
         k2 = round(
             (h * func2(Expression, matrix[i+1][0], (k1 + (matrix[i][1])))), fix)
@@ -40,14 +57,14 @@ def ModifiedEuler():
         print(row_format.format("", *row))
 
 
-def Huen():
-    Expression = input(
-        "Enter the Function to Integrate using Huen Method : ")
-    h = float(input("Enter h : "))
-    n = bound1 = float(input("From a = "))
-    bound2 = float(input("To b = "))
-    y0 = float(input("Enter y("+str(bound1)+") : "))
-    fix = int(input("Enter the rounding point digits : "))
+def Huen(Expression, h, n, bound1, bound2, y0, fix):
+    # Expression = input(
+    #     "Enter the Function to Integrate using Huen Method : ")
+    # h = float(input("Enter h : "))
+    # n = bound1 = float(input("From a = "))
+    # bound2 = float(input("To b = "))
+    # y0 = float(input("Enter y("+str(bound1)+") : "))
+    # fix = int(input("Enter the rounding point digits : "))
 
     matrix = []
     while (n <= bound2):
@@ -76,13 +93,13 @@ def Huen():
         print(row_format.format("", *row))
 
 
-def Euler():
-    Expression = input("Enter the Function to Integrate using Huen Method : ")
-    h = float(input("Enter h : "))
-    n = bound1 = float(input("From a = "))
-    bound2 = float(input("To b = "))
-    y0 = float(input("Enter y("+str(bound1)+") : "))
-    fix = int(input("Enter the rounding point digits : "))
+def Euler(Expression, h, n, bound1, bound2, y0, fix):
+    # Expression = input("Enter the Function to Integrate using Huen Method : ")
+    # h = float(input("Enter h : "))
+    # n = bound1 = float(input("From a = "))
+    # bound2 = float(input("To b = "))
+    # y0 = float(input("Enter y("+str(bound1)+") : "))
+    # fix = int(input("Enter the rounding point digits : "))
 
     matrix = []
     while (n <= bound2):
@@ -107,14 +124,14 @@ def Euler():
         print(row_format.format("", *row))        
 
 
-def Rk4():
-    Expression = input(
-        "Enter the Function to Integrate using Huen Method : ")
-    h = float(input("Enter h : "))
-    n = bound1 = float(input("From a = "))
-    bound2 = float(input("To b = "))
-    y0 = float(input("Enter y("+str(bound1)+") : "))
-    fix = int(input("Enter the rounding point digits : "))
+def Rk4(Expression, h, n, bound1, bound2, y0, fix):
+    # Expression = input(
+    #     "Enter the Function to Integrate using Huen Method : ")
+    # h = float(input("Enter h : "))
+    # n = bound1 = float(input("From a = "))
+    # bound2 = float(input("To b = "))
+    # y0 = float(input("Enter y("+str(bound1)+") : "))
+    # fix = int(input("Enter the rounding point digits : "))
 
     matrix = []
     while (n <= bound2):
@@ -146,4 +163,4 @@ def Rk4():
 # ModifiedEuler()
 # Huen()
 # Euler()
-Rk4()
+enterdata()
