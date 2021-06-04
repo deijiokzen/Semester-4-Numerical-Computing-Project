2308042f66a4d16564bb0b1cfa8659e6053bc58d
Batman27801 <57960455+Teakay23@users.noreply.github.com>
2021-06-04 11:30:10 +0500

    Added error columns

--- chap5methods.py
+++ chap5methods.py
@@ -8,20 +8,21 @@ def enterdata_():
     bound2 = float(input("To b = "))
     y0 = float(input("Enter y("+str(bound1)+") : "))
     fix = int(input("Enter the rounding point digits : "))
+    sol = input("Enter solution of IVP for error checking: ")
 
     choice = int(input("Which methods do you want to use for Integrating : \n1. Euler.\n2. Modified Euler.\n3. Huen Method.\n4. RK4 Method\n Choice : "))
     if choice == 1:
-        Euler(Expression, h, n, bound1, bound2, y0, fix)
+        Euler(Expression, h, n, bound1, bound2, y0, fix, sol)
     elif choice == 2:
-        ModifiedEuler(Expression, h, n, bound1, bound2, y0, fix)
+        ModifiedEuler(Expression, h, n, bound1, bound2, y0, fix, sol)
     elif choice == 3:
-        Huen(Expression, h, n, bound1, bound2, y0, fix)
+        Huen(Expression, h, n, bound1, bound2, y0, fix, sol)
     elif choice == 4:
-        Rk4(Expression, h, n, bound1, bound2, y0, fix)
+        Rk4(Expression, h, n, bound1, bound2, y0, fix, sol)
     else:
         print("no choice allocated to this number.")
 
-def ModifiedEuler(Expression, h, n, bound1, bound2, y0, fix):
+def ModifiedEuler(Expression, h, n, bound1, bound2, y0, fix, sol):
     # Expression = input(
     #     "Enter the Function to Integrate using Modified Euler Method : ")
     # h = float(input("Enter h : "))
@@ -32,7 +33,7 @@ def ModifiedEuler(Expression, h, n, bound1, bound2, y0, fix):
 
     matrix = []
     while (n <= bound2):
-        list = [n, y0]
+        list = [round(n, fix), y0, round(func(sol, n), fix), 0]
         n += h
         matrix.append(list)
 
@@ -44,18 +45,26 @@ def ModifiedEuler(Expression, h, n, bound1, bound2, y0, fix):
             (h * func2(Expression, matrix[i+1][0], (k1 + (matrix[i][1])))), fix))
 
         matrix[i+1][1] = round((matrix[i][1] + 0.5*(k1 + k2)), fix)
+        matrix[i+1][3] = round(abs(matrix[i+1][2] - matrix[i+1][1]), fix)
 
         i += 1
         n += h
 
-    header = ["x", "y"]
+    if Expression.find("x") != -1:
+        x = "x"
+    if Expression.find("t") != -1:
+        x = "t"
+    else:
+        x = "val"
+
+    header = [x, "Modified Euler", "True Value", "Abs Error"]
     row_format = "{:>15}" * (len(header) + 1)
     print(row_format.format("", *header))
     for row in matrix:
         print(row_format.format("", *row))
 
 
-def Huen(Expression, h, n, bound1, bound2, y0, fix):
+def Huen(Expression, h, n, bound1, bound2, y0, fix, sol):
     # Expression = input(
     #     "Enter the Function to Integrate using Huen Method : ")
     # h = float(input("Enter h : "))
@@ -66,7 +75,7 @@ def Huen(Expression, h, n, bound1, bound2, y0, fix):
 
     matrix = []
     while (n <= bound2):
-        list = [n, y0]
+        list = [round(n, fix), y0, round(func(sol, n), fix), 0]
         n += h
         matrix.append(list)
 
@@ -80,18 +89,26 @@ def Huen(Expression, h, n, bound1, bound2, y0, fix):
             Expression, (((2*h)/3)+matrix[i][0]), (((2*k2*h)/3) + (matrix[i][1]))), fix))
 
         matrix[i+1][1] = round((matrix[i][1] + (1/4)*(k1 + (3*k3))*h), fix)
+        matrix[i+1][3] = round(abs(matrix[i+1][2] - matrix[i+1][1]), fix)
 
         i += 1
         n += h
 
-    header = ["x", "y"]
+    if Expression.find("x") != -1:
+        x = "x"
+    if Expression.find("t") != -1:
+        x = "t"
+    else:
+        x = "val"
+
+    header = [x, "Huen", "True Value", "Error"]
     row_format = "{:>15}" * (len(header) + 1)
     print(row_format.format("", *header))
     for row in matrix:
         print(row_format.format("", *row))
 
 
-def Euler(Expression, h, n, bound1, bound2, y0, fix):
+def Euler(Expression, h, n, bound1, bound2, y0, fix, sol):
     # Expression = input("Enter the Function to Integrate using Huen Method : ")
     # h = float(input("Enter h : "))
     # n = bound1 = float(input("From a = "))
@@ -101,7 +118,7 @@ def Euler(Expression, h, n, bound1, bound2, y0, fix):
 
     matrix = []
     while (n <= bound2):
-        list = [n, y0]
+        list = [round(n, fix), y0, round(func(sol, n), fix), 0]
         n += h
         matrix.append(list)
 
@@ -111,18 +128,26 @@ def Euler(Expression, h, n, bound1, bound2, y0, fix):
         k1 = float(func2(Expression, matrix[i][0], matrix[i][1]))
         
         matrix[i+1][1] = round((matrix[i][1] + (h*k1)), fix)
+        matrix[i+1][3] = round(abs(matrix[i+1][2] - matrix[i+1][1]), fix)
 
         i += 1
         n += h
 
-    header = ["x", "y"]
+    if Expression.find("x") != -1:
+        x = "x"
+    if Expression.find("t") != -1:
+        x = "t"
+    else:
+        x = "val"
+
+    header = [x, "Euler", "True Value", "Error"]
     row_format = "{:>15}" * (len(header) + 1)
     print(row_format.format("", *header))
     for row in matrix:
         print(row_format.format("", *row))        
 
 
-def Rk4(Expression, h, n, bound1, bound2, y0, fix):
+def Rk4(Expression, h, n, bound1, bound2, y0, fix, sol):
     # Expression = input(
     #     "Enter the Function to Integrate using Huen Method : ")
     # h = float(input("Enter h : "))
@@ -133,7 +158,7 @@ def Rk4(Expression, h, n, bound1, bound2, y0, fix):
 
     matrix = []
     while (n <= bound2):
-        list = [n, y0]
+        list = [round(n, fix), y0, round(func(sol, n), fix), 0]
         n += h
         matrix.append(list)
 
@@ -147,11 +172,19 @@ def Rk4(Expression, h, n, bound1, bound2, y0, fix):
 
 
         matrix[i+1][1] = round((matrix[i][1] + (1/6)*(k1 + 2*(k2+k3) + k4)), fix)
+        matrix[i+1][3] = round(abs(matrix[i+1][2] - matrix[i+1][1]), fix)
 
         i += 1
         n += h
 
-    header = ["x", "y"]
+    if Expression.find("x") != -1:
+        x = "x"
+    if Expression.find("t") != -1:
+        x = "t"
+    else:
+        x = "val"
+
+    header = [x, "RK4", "True Value", "Error"]
     row_format = "{:>15}" * (len(header) + 1)
     print(row_format.format("", *header))
     for row in matrix:
