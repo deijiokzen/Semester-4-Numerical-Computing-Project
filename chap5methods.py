23faafff3c4f475409d5da332f6631007108bc90
Batman27801 <57960455+Teakay23@users.noreply.github.com>
2021-06-04 12:27:25 +0500

    added chap 5 midpoint

--- chap5methods.py
+++ chap5methods.py
@@ -10,7 +10,7 @@ def enterdata_():
     fix = int(input("Enter the rounding point digits : "))
     sol = input("Enter solution of IVP for error checking: ")
 
-    choice = int(input("Which methods do you want to use for Integrating : \n1. Euler.\n2. Modified Euler.\n3. Huen Method.\n4. RK4 Method\n Choice : "))
+    choice = int(input("Which methods do you want to use for Integrating : \n1. Euler.\n2. Modified Euler.\n3. Huen Method.\n4. RK4 Method\n5. Midpoint\n Choice : "))
     if choice == 1:
         Euler(Expression, h, n, bound1, bound2, y0, fix, sol)
     elif choice == 2:
@@ -19,6 +19,8 @@ def enterdata_():
         Huen(Expression, h, n, bound1, bound2, y0, fix, sol)
     elif choice == 4:
         Rk4(Expression, h, n, bound1, bound2, y0, fix, sol)
+    elif choice == 5:
+        Midpoint_(Expression, h, n, bound1, bound2, y0, fix, sol)
     else:
         print("no choice allocated to this number.")
 
@@ -190,6 +192,46 @@ def Rk4(Expression, h, n, bound1, bound2, y0, fix, sol):
     for row in matrix:
         print(row_format.format("", *row))
 
+def Midpoint_(Expression, h, n, bound1, bound2, y0, fix, sol):
+    # Expression = input(
+    #     "Enter the Function to Integrate using Modified Euler Method : ")
+    # h = float(input("Enter h : "))
+    # n = bound1 = float(input("From a = "))
+    # bound2 = float(input("To b = "))
+    # y0 = float(input("Enter y("+str(bound1)+") : "))
+    # fix = int(input("Enter the rounding point digits : "))
+
+    matrix = []
+    while (n <= bound2):
+        list = [round(n, fix), y0, round(func(sol, n), fix), 0]
+        n += h
+        matrix.append(list)
+
+    n = bound1+h
+    i = int(0)
+    while (n <= bound2):
+        k1 = float(round((func2(Expression, matrix[i][0], matrix[i][1])), fix))
+        k2 = float(round(
+            (func2(Expression, matrix[i][0] + (h/2), ((h/2)*k1 + (matrix[i][1])))), fix))
+
+        matrix[i+1][1] = round((matrix[i][1] + h*(k2)), fix)
+        matrix[i+1][3] = round(abs(matrix[i+1][2] - matrix[i+1][1]), fix)
+
+        i += 1
+        n += h
+
+    if Expression.find("x") != -1:
+        x = "x"
+    elif Expression.find("t") != -1:
+        x = "t"
+    else:
+        x = "val"
+
+    header = [x, "Midpoint", "y("+x+")", "Abs Error"]
+    row_format = "{:>15}" * (len(header) + 1)
+    print(row_format.format("", *header))
+    for row in matrix:
+        print(row_format.format("", *row))
 
 # ModifiedEuler()
 # Huen()
